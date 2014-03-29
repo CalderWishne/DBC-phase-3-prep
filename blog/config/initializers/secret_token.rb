72e088266d48590e781720f605b35e8095245d6e
Batman <Bruce@wayne.enterprises.com>
2014-03-29 16:54:43 -0500

    added blog skeleton

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '3b29dc265f5efaf09bf78c68945902b85ca72631af851204a513b5024b5efa37ef710ecf5b4bdc3c8a08fc6c2810603ef1a265404a90db8621c1bb05254ec0d3'
